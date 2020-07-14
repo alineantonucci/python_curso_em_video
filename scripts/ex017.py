63c7b43b6bac984a3c06bd49ecb28e9bcb20f798
Aline Antonucci <68189868+alineantonucci@users.noreply.github.com>
2020-07-13 22:17:56 -0300

    Add files via upload

---
+++ scripts/ex017.py
@@ -0,0 +1,9 @@
+""" Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo
+"""
+
+cateto_adjacente = float(input('Digite o cateto adjacente: '))
+cateto_oposto = float(input('Digite cateto oposto: '))
+catetos = (cateto_adjacente ** 2) + (cateto_oposto ** 2)
+hipotenusa = (catetos)**(1/2)
+print('A hipotenusa é: {:.2f}'
+      .format(hipotenusa))
