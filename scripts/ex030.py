2f9bc8bd92b20620c77e22be16a6447cbb443d53
Aline Antonucci <68189868+alineantonucci@users.noreply.github.com>
2020-08-03 20:46:51 -0300

    Add files via upload

---
+++ scripts/ex030.py
@@ -0,0 +1,8 @@
+"""Programa que lê um número inteiro e mostre se ele é PAR ou ÍMPAR
+"""
+
+numero = int(input('Digite um número inteiro: '))
+if numero % 2 == 0:
+    print('Seu número é PAR!')
+else:
+    print('Seu núemro é ÍMPAR!')
