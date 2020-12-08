5c09011199f860ff28f82ceacac558ca711d261e
Aline Antonucci <68189868+alineantonucci@users.noreply.github.com>
2020-12-08 06:00:11 -0300

    Add files via upload

---
+++ scripts/ex045.py
@@ -0,0 +1,49 @@
+"""Jogo de JO KEN PO com o usuário
+"""
+#importar randomização
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('Vamos JOGAR')
+print("""Sus opções:
+[0] PEDRA
+[1] PAPEL
+[2] TESOURA """)# opções de jogada
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO')
+print('-=' * 15)
+print('O computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('-=' * 15)
+if  computador == 0: # coputador jogou PEDRA
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA')
+elif computador == 1: # computador jogou PAPEL
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADA IVÁLIDA')
+elif computador == 2: #computador jogou TESOURA
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVÁLIDA')
